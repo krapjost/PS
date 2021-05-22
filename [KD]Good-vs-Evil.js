012de39cb33fce7786fda433c3d2eb4e86ddf15d
krapjost <krapjost@gmail.com>
2021-05-22 14:24:45 +0900

    문제 추가

---
+++ [KD]Good-vs-Evil.js
@@ -0,0 +1,28 @@
+function goodVsEvil(good, evil){
+  const gv = {0:1,1:2,2:3,3:3,4:4,5:10}
+  const ev = {0:1,1:2,2:2,3:2,4:3,5:5,6:10}
+  
+  const ga = good.split(' '), ea = evil.split(' ');
+  
+  let es = 0, gs =0;
+  for (let i=0; i < ea.length; i++){
+    if (ea[i]>0){
+      es += ev[i]*ea[i]
+    }
+  }
+  for (let i=0; i < ga.length; i++){
+    if (ga[i]>0){
+      gs += gv[i]*ga[i]
+    }
+  }
+  if (gs===es) {
+    return 'Battle Result: No victor on this battle field'
+  }
+  if (gs > es) {
+    return 'Battle Result: Good triumphs over Evil'
+  } else {
+    return 'Battle Result: Evil eradicates all trace of Good'
+  }
+}
+
+console.log(goodVsEvil('1 1 1 1 1 1', '1 0 0 3 4 0 0'))
